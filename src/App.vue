ee1e6ac9c5ff199dfad5eef8efc7bcb9f3e15da1
Arian Krasniqi <ariiankrasniqi@gmail.com>
2020-01-24 03:10:07 +0100

    Header Layout Done

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <app-header></app-header>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-export default {};
+import Header from "./components/Header.vue";
+
+export default {
+  components: {
+    appHeader: Header
+  }
+};
 </script>
 
-<style></style>
+<style>
+body {
+  padding: 30px;
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,56 @@
 <template>
-  <h1>
-    The Header Component
-  </h1>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+    <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+    <!-- Mobile Menu -->
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav mr-auto">
+        <router-link to="/portfolio" activeClass="active" tag="li">
+          <a class="nav-link">Portfolio</a>
+        </router-link>
+        <router-link to="/stocks" activeClass="active" tag="li">
+          <a class="nav-link">Stocks</a>
+        </router-link>
+      </ul>
+
+      <span class="navbar-text">
+        <ul class="navbar-nav mr-auto">
+          <a href="endday" class="nav-link" tag="li">End Day</a>
+          <a
+            class="nav-link"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+            tag="li"
+          >Save and Load</a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </ul>
+      </span>
+
+      <form class="form-inline my-2 mr-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
 </template>
