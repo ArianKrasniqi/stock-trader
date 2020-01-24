a7a24288af35d20c21d471c8d2810c04df3f2710
Arian Krasniqi <ariiankrasniqi@gmail.com>
2020-01-24 04:43:28 +0100

    Stocks Displayed

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,5 +1,46 @@
 <template>
-  <h1>
-    The Stocks Stock Component
-  </h1>
+  <div class="col-sm-4 stock">
+    <div class="card border-success mb-3" style="max-width: 22rem;">
+      <div class="card-header">
+        {{stock.name}}
+        <small>(Price: {{stock.price}})</small>
+      </div>
+      <div class="card-body text-success">
+        <div class="float-left">
+          <input type="number" class="form-control" placeholder="Quantity" v-model="quantity" />
+        </div>
+        <div class="float-right">
+          <button class="btn btn-success" @click="buyStock" :disabled="quantity <= 0">Buy</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockName: this.stock.name,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      };
+      this.quantity = 0;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.stock {
+  margin-top: 20px;
+}
+</style>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,5 +1,33 @@
 <template>
-  <h1>
-    The Stocks Stocks Component
-  </h1>
+  <div class="stocks">
+    <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+  </div>
 </template>
+
+<script>
+import Stock from "./Stock.vue";
+
+export default {
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: "Audi", price: 100 },
+        { id: 1, name: "Google", price: 200 },
+        { id: 1, name: "Apple", price: 250 },
+        { id: 1, name: "Facebook", price: 180 }
+      ]
+    };
+  },
+  components: {
+    appStock: Stock
+  }
+};
+</script>
+
+<style scoped>
+.stocks {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+</style>
